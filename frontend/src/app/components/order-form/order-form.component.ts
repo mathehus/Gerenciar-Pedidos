f94d213ed32b9f70a72ba625d8172e4edfd1fbee
Matheus Santos <matheusqsantos23@gmail.com>
2025-03-17 12:22:01 -0300

    Atualização Desing

--- frontend/src/app/components/order-form/order-form.component.ts
+++ frontend/src/app/components/order-form/order-form.component.ts
@@ -2,18 +2,32 @@ import { Component } from '@angular/core';
 import { OrderService } from '../../services/order.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-order-form',
   templateUrl: './order-form.component.html',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatCardModule,
+    MatListModule],
   styleUrls: ['./order-form.component.scss']
 })
 export class OrderFormComponent {
   customer = '';
   items = [{ product: '', quantity: 1, price: 0 }];
 
-  constructor(private orderService: OrderService) {}
+  constructor(private orderService: OrderService) { }
 
   addItem() {
     this.items.push({ product: '', quantity: 1, price: 0 });
--- frontend/src/app/components/order-list/order-list.component.ts
+++ frontend/src/app/components/order-list/order-list.component.ts
@@ -2,17 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { OrderService } from '../../services/order.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-order-list',
   templateUrl: './order-list.component.html',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule,
+    MatCardModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule],
   styleUrls: ['./order-list.component.scss']
 })
 export class OrderListComponent implements OnInit {
   orders: any[] = [];
 
-  constructor(private orderService: OrderService) {}
+  constructor(private orderService: OrderService) { }
 
   ngOnInit() {
     this.fetchOrders();
