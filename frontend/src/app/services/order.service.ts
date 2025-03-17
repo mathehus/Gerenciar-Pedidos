be51901a85f5095b9a0f6870c205f2941bc21d8b
Matheus Santos <matheusqsantos23@gmail.com>
2025-03-17 11:02:51 -0300

    tokenBackend

--- frontend/src/app/services/order.service.ts
+++ frontend/src/app/services/order.service.ts
@@ -2,48 +2,31 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OrderService {
   private apiUrl = 'http://localhost:5000/orders';
-  private loginUrl = 'http://localhost:5000/login';
-  private token: string | null = null; // Variável para armazenar o token
+  private token = 'TOKEN-FIXO-NO-CODIGO'; // Token fixo
 
   constructor(private http: HttpClient) {}
 
-  // Método para fazer login e obter o token
-  login(username: string, password: string): Observable<any> {
-    const credentials = { username, password };
-    return this.http.post<any>(this.loginUrl, credentials);
-  }
-
-  // Método para definir o token
-  setToken(token: string): void {
-    this.token = token;
-  }
-
-  // Método para obter os headers com o token
   private getHeaders(): HttpHeaders {
-    if (!this.token) {
-      throw new Error('Token não disponível. Faça login primeiro.');
-    }
     return new HttpHeaders().set('Authorization', `Bearer ${this.token}`);
   }
 
   getOrders(): Observable<any[]> {
-    const headers = this.getHeaders(); // Obtém os headers com o token
+    const headers = this.getHeaders();
     return this.http.get<any[]>(this.apiUrl, { headers });
   }
 
   createOrder(order: any): Observable<any> {
-    const headers = this.getHeaders(); // Obtém os headers com o token
+    const headers = this.getHeaders();
     return this.http.post<any>(this.apiUrl, order, { headers });
   }
 
   updateOrderStatus(id: string, status: string): Observable<any> {
-    const headers = this.getHeaders(); // Obtém os headers com o token
+    const headers = this.getHeaders();
     return this.http.patch<any>(`${this.apiUrl}/${id}`, { status }, { headers });
   }
 }
